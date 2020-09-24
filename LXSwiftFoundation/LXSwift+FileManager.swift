dade6a244d4592a7015fb97418dcba0c144a710e
lixiang <lixiang@gokuaidian.com>
2020-09-24 11:55:20 +0800

    FileManager

---
+++ LXSwiftFoundation/LXSwift+FileManager.swift
@@ -0,0 +1,41 @@
+//
+//  LXSwift+FileManager.swift
+//  LXSwiftFoundationModule
+//
+//  Created by XL on 2020/9/24.
+//  Copyright © 2020 李响. All rights reserved.
+//
+
+import UIKit
+
+extension FileManager: LXCompatible { }
+
+//MARK: -  Extending methods for Date
+extension LXSwiftBasics where Base == Dictionary<String, Any> {
+    
+    /// Caches
+    public static var cachesURL: URL? {
+        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).last
+    }
+    public static var cachesPath: String? {
+        return NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).first
+    }
+    
+    /// "Documents"
+    public static var documentURL: URL {
+        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last!
+    }
+    public static var documentPath: String {
+        return NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+    }
+    
+    /// "Library"
+    public static var libraryURL: URL? {
+        return FileManager.default.urls(for: .libraryDirectory, in: .userDomainMask).last
+    }
+    public static var libraryPath: String? {
+        return NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true).first
+    }
+    
+    
+}
