a490ac025a2dc76f440b28d3da6f1ad8995b5b87
lixiang01 <lixiang01@meishubao.com>
2021-08-28 14:39:21 +0800

    恢复存储

--- LXSwiftFoundation.podspec
+++ LXSwiftFoundation.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |spec|
   #
 
   spec.name         = "LXSwiftFoundation"
-  spec.version      = "8.5.3"
+  spec.version      = "8.5.4"
   spec.summary      = "Extend more user-friendly functions for system classes"
 
   
--- LXSwiftFoundation/swiftTools/LXSwiftStorage.swift
+++ LXSwiftFoundation/swiftTools/LXSwiftStorage.swift
@@ -7,52 +7,19 @@
 
 import UIKit
 
+public struct  LXSwiftStorage: LXSwiftCompatible { }
 
-/*
- 字符串简单用法
- LXSwiftStorage.setStorage("sasasa", key: "ss")
- let m = LXSwiftStorage<String>.getStorage(for: "ss")
-  */
+/// 扩展存储 并且加密
+extension LXSwiftBasics where Base == LXSwiftStorage {
 
-
-
-/*
- 模型用法
- class Model: Codable {
-     var aa: String = "fd"
-     var dd: Int = 0
- }
-
- let model = Model()
- LXSwiftStorage.setStorage("model", key: "ss")
- 
- let m = LXSwiftStorage<Model>.getStorage(for: "ss")
-*/
-
-public struct  LXSwiftStorage<T: Codable>: LXSwiftCompatible {
-   
-    /// 数据编码后存储
-    public static func set(_ value: T, key: String) where T: Encodable {
-        if let data = try? JSONEncoder().encode(value) {
-            setStorage(with: data, key: key)
-        }
-    }
-    
-    /// 数据取出后解码
-    public static func get(for key: String) -> T? where T: Decodable {
-        guard let data = getStorage(for: key) as? Data else { return nil }
-        let decodeValue = try? JSONDecoder().decode(T.self, from: data)
-        return decodeValue
-    }
-    
-    /// 正常存储
-    internal static func setStorage(with value: Any?, key: String) {
+    /// 存储
+    public static func set(with value: Any?, key: String) {
         UserDefaults.standard.set(value, forKey: key)
         UserDefaults.standard.synchronize()
     }
     
-    /// 正常取值
-    internal static func getStorage(for key: String) -> Any? {
+    /// 取值
+    public static func get(for key: String) -> Any? {
         return UserDefaults.standard.object(forKey: key)
     }
     
