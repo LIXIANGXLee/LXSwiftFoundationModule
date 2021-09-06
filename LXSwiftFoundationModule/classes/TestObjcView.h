4b48f3d41827d976c1e052fc75bbe10ee61990c4
lixiang <lixiang@gokuaidian.com>
2021-09-06 22:52:41 +0800

    优化

---
+++ LXSwiftFoundationModule/classes/TestObjcView.h
@@ -0,0 +1,21 @@
+//
+//  TestObjcView.h
+//  LXSwiftFoundationModule
+//
+//  Created by Mac on 2021/9/4.
+//  Copyright © 2021 李响. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <LXSwiftFoundation/LXSwiftFoundation-Swift.h>
+
+#import <LXSwiftFoundation/LXObjcFoundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestObjcView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LXSwiftFoundationModule/classes/TestObjcView.m
@@ -0,0 +1,44 @@
+//
+//  TestObjcView.m
+//  LXSwiftFoundationModule
+//
+//  Created by Mac on 2021/9/4.
+//  Copyright © 2021 李响. All rights reserved.
+//
+
+#import "TestObjcView.h"
+
+@implementation TestObjcView
+
++ (void)load {
+
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [[self alloc] init];
+    });
+    
+}
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+               
+        
+        NSLog(@"-=-=-=-=-=-%f",SCREEN_HEIGHT_TO_HEIGHT);
+        NSLog(@"-=-=-=-=-=-%f",SCREEN_WIDTH_TO_WIDTH);
+        NSLog(@"-=-=-=-=-=-%f",SCREEN_HEIGHT_TO_NAVBARHEIGHT);
+        NSLog(@"-=-=-=-=-=-%f",SCREEN_HEIGHT_TO_STATUSHEIGHT);
+        NSLog(@"-=-=-=-=-=-%f",SCREEN_HEIGHT_TO_TOUCHBARHEIGHT);
+        NSLog(@"-=-=-=-=-=-%f",SCREEN_HEIGHT_TO_TABBARHEIGHT);
+        NSLog(@"-=-=-=-=-=-%f",SCALE_IP6_HEIGHT_TO_HEIGHT(20));
+        NSLog(@"-=-=-=-=-=-%f",SCALE_IPAD129_WIDTH_TO_WIDTH(20));
+        NSLog(@"-=-=-=-=-=-%f",SCALE_IPAD129_HEIGHT_TO_HEIGHT(20));
+        NSLog(@"-=-=-=-=-=-%f",SCALE_IP6_WIDTH_TO_WIDTH(20));
+        NSLog(@"-=-=-=-=-=-%f", [LXObjcApp statusBarHeight]);
+
+    }
+    return self;
+}
+
+
+@end
